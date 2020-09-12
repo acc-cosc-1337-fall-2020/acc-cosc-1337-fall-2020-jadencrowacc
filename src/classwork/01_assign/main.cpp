a265e66aa173be1ba2bd0c741302f247069771eb
Jaden Crow <crowjaden@gmail.com>
2020-09-11 23:09:41 -0500

    Add code for main method

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,20 @@
 //Write the include statement for types.h here
-
+#include <types.h>
 //Write include for capturing input from keyboard and displaying output to screen
+#include <iostream>
 
 
-
-int main() 
+int main()
 {
+    int num;
+    std::cin >> num;
 
-	return 0;
+    int result = multiply_numbers(num);
+    std::cout << "result: " << result;
+
+    int num1 = 4;
+
+    result = multiply_numbers(num1);
+    std::cout << "result: " << result;
 }
 
