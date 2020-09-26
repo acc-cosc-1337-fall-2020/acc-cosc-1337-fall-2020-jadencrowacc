86edf7fa359f94456ac995782d76bf366d587b3a
Jaden Crow <crowjaden@gmail.com>
2020-09-25 23:29:17 -0500

    Create main

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,14 @@
 //Write the include statement for decisions.h here
-
+#include <decision.h>
 //Write namespace using statements for cout and cin
+#include <iostream>
 
-int main() 
-{
+using namespace std;
 
-	return 0;
+int main() {
+    int grade;
+    cout << "Numeric Grade: ";
+    cin >> grade;
+    cout << "Letter Grade: " << get_letter_grade_using_if(grade) << "\n";
 }
 
