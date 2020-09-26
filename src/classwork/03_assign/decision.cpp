601834aa8d9f73f5f9f68a49d32bb48ba9d24a80
Jaden Crow <crowjaden@gmail.com>
2020-09-25 23:28:19 -0500

    Create letter grade functions

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,33 @@
 //cpp
+#include "decision.h"
+
+char get_letter_grade_using_if(int grade) {
+    if (grade > 90)
+        return 'A';
+    if (grade > 80)
+        return 'B';
+    if (grade > 70)
+        return 'C';
+    if (grade > 60)
+        return 'D';
+    return 'F';
+    return 0;
+}
+
+char get_letter_grade_using_switch(int grade) {
+    switch (grade / 10) {
+        case 10:
+            return 'A';
+        case 9:
+            return 'A';
+        case 8:
+            return 'B';
+        case 7:
+            return 'C';
+        case 6:
+            return 'D';
+        default:
+            return 'F';
+    }
+}
+
--- src/classwork/03_assign/decision.h
+++ src/classwork/03_assign/decision.h
@@ -1,1 +1,5 @@
 //header
+char get_letter_grade_using_if(int grade);
+
+char get_letter_grade_using_switch(int grade);
+
