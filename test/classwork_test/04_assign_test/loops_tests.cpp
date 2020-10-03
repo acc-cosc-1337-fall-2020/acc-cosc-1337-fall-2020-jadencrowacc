d22e28c85c7ac745f21caf21f45b0d8f65fb719c
Jaden Crow <crowjaden@gmail.com>
2020-10-03 00:05:49 -0500

    Create factorial tests

--- test/classwork_test/04_assign_test/loops_tests.cpp
+++ test/classwork_test/04_assign_test/loops_tests.cpp
@@ -1,8 +1,14 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
 #include "catch.hpp"
 #include "loops.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+    REQUIRE(true == true);
 }
 
+TEST_CASE("Test factorial") {
+    REQUIRE(factorial(3));
+    REQUIRE(factorial(4));
+    REQUIRE(factorial(5));
+}
