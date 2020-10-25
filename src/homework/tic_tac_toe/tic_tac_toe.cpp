474cd88c2006faa79840666b421469e7f566303e
Jaden Crow <crowjaden@gmail.com>
2020-10-24 23:56:19 -0500

    Add tictactoe functions

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,59 @@
 //cpp
+
+#include "tic_tac_toe.h"
+#include <iostream>
+#include <string>
+
+using std::string;
+
+using std::cout;
+
+bool tic_tac_toe::game_over() {
+    for (int i = 0; i <= 6; i += 3)
+        if (board[0 + i] != " " && board[0 + i] == board[1 + i] && board[1 + i] == board[2 + i])
+            return true;
+    return false;
+}
+
+void tic_tac_toe::start_game(string first_player) {
+    player = first_player;
+}
+
+bool tic_tac_toe::mark_board(int position) {
+    if(board[position-1] != " " || position < 1 || position > 9)
+        return false;
+    board[position - 1] = player;
+    set_next_player();
+    return true;
+}
+
+string tic_tac_toe::get_player() {
+    return player;
+}
+
+void tic_tac_toe::display_board() {
+    for (int i = 1; i <= board.size(); i++) {
+        cout << board[i - 1];
+        if (i % 3 == 0)
+            cout << "\n";
+    }
+}
+
+void tic_tac_toe::set_next_player() {
+    if (player == "X")
+        player = "O";
+    else
+        player = "X";
+}
+
+bool tic_tac_toe::check_board_full() {
+    for (string peg : board)
+        if (peg == " ")
+            return false;
+    return true;
+}
+
+void tic_tac_toe::clear_board() {
+    for (int i = 0; i < board.size(); i++)
+        board[i] = " ";
+}
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,34 @@
-//h
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class tic_tac_toe
+{
+public:
+
+    bool game_over();
+
+    void start_game(string first_player);
+
+    bool mark_board(int position);
+
+    string get_player();
+
+    void display_board();
+
+    void set_next_player();
+
+    bool check_board_full();
+
+    void clear_board();
+
+private:
+
+    string player;
+    vector<string> board = {" "," "," ",
+                            " "," "," ",
+                            " "," "," ",};
+};
