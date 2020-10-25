6bf694fccf95ce59c80e9dfb7f96a2114328daa6
Jaden Crow <crowjaden@gmail.com>
2020-10-25 00:18:36 -0500

    Add main

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,35 @@
+#include <tic_tac_toe.h>
+#include <iostream>
+#include <string>
 
-int main() 
-{
+using std::cout;
+using std::cin;
+using std::string;
 
-	return 0;
+int main() {
+    tic_tac_toe game;
+    cout << "Please type X or O to determine who will go first: ";
+    string first_player = "";
+    while (first_player != "X" && first_player != "O") {
+        cin >> first_player;
+        if (first_player != "X" && first_player != "O")
+            cout << "Invalid input, please type X or O: ";
+    }
+    game.start_game(first_player);
+
+    while (!game.game_over()) {
+        cout << game.get_player() << " it is your turn, type a number 1-9 to place you peg: ";
+
+        int position = 0;
+
+        cin >> position;
+        while (!game.mark_board(position)) {
+            cout << "Invalid input, please type a number 1-9: ";
+            cin >> position;
+        }
+        cout << "\n===\n";
+        game.display_board();
+        cout << "===\n\n";
+    }
+    return 0;
 }
