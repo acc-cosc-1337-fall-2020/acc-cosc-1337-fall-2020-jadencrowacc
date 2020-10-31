1ab4ab49969ce565a447b92cef7f30c5bc42d47b
Jaden Crow <crowjaden@gmail.com>
2020-10-30 23:28:34 -0500

    Add looping functionality to main and print winner

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -8,28 +8,39 @@ using std::string;
 
 int main() {
     tic_tac_toe game;
-    cout << "Please type X or O to determine who will go first: ";
-    string first_player = "";
-    while (first_player != "X" && first_player != "O") {
-        cin >> first_player;
-        if (first_player != "X" && first_player != "O")
-            cout << "Invalid input, please type X or O: ";
-    }
-    game.start_game(first_player);
+    string play_again = "Y";
+    while (play_again == "Y") {
 
-    while (!game.game_over()) {
-        cout << game.get_player() << " it is your turn, type a number 1-9 to place you peg: ";
-
-        int position = 0;
-
-        cin >> position;
-        while (!game.mark_board(position)) {
-            cout << "Invalid input, please type a number 1-9: ";
-            cin >> position;
+        string first_player;
+        while (first_player != "X" && first_player != "O") {
+            cout << "Please type X or O to determine who will go first: ";
+            cin >> first_player;
         }
-        cout << "\n===\n";
-        game.display_board();
-        cout << "===\n\n";
+
+        game.start_game(first_player);
+
+        while (!game.game_over()) {
+            cout << game.get_player() << " it is your turn, type a number 1-9 to place you peg: ";
+
+            int position = 0;
+
+            cin >> position;
+            while (!game.mark_board(position)) {
+                cout << "Invalid input, please type a number 1-9: ";
+                cin >> position;
+            }
+            cout << "\n===\n";
+            game.display_board();
+            cout << "===\n\n";
+        }
+        if (game.get_winner() == "C")
+            cout << "Tie.\n\n";
+        else
+            cout << game.get_winner() << " wins!\n\n";
+
+        do {
+            cout << "Would you like to play again? (Y/N): ";
+            cin >> play_again;
+        } while (play_again != "Y" && play_again != "N");
     }
-    return 0;
 }
