7dbab47d84e187ed354b2ee1cc3c2240ec719467
Jaden Crow <crowjaden@gmail.com>
2020-11-08 00:05:26 -0600

    Add tic_tac_toe_manager

--- src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,31 @@
 //cpp
+
+#include "tic_tac_toe_manager.h"
+#include <string>
+
+std::ostream &operator<<(std::ostream &out, const tic_tac_toe_manager &manager) {
+    for (const tic_tac_toe &game : manager.games)
+        out << game;
+    return out;
+}
+
+void tic_tac_toe_manager::save_game(tic_tac_toe game) {
+    games.push_back(game);
+    switch (game.get_winner()[0]) {
+        case 'O':
+            o_wins++;
+            break;
+        case 'X':
+            x_wins++;
+            break;
+        case 'C':
+            ties++;
+            break;
+    }
+}
+
+void tic_tac_toe_manager::get_winner_total(int &o, int &x, int &t) {
+    o = o_wins;
+    x = x_wins;
+    t = ties;
+}
--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,17 @@
 //h
+#include <vector>
+#include "tic_tac_toe.h"
+
+class tic_tac_toe_manager {
+private:
+    std::vector<tic_tac_toe> games;
+    int x_wins = 0;
+    int o_wins = 0;
+    int ties = 0;
+public:
+    friend std::ostream &operator<<(std::ostream &out, const tic_tac_toe_manager &manager);
+
+    void save_game(tic_tac_toe game);
+
+    void get_winner_total(int &o, int &x, int &t);
+};
