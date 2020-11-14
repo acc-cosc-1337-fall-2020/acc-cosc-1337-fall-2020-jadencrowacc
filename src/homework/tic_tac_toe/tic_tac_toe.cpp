74a1c05dfc05e0e81f3f2da8554f62b80de6ad68
Jaden Crow <crowjaden@gmail.com>
2020-11-14 01:45:17 -0600

    Make cin and cout overloads work with 4x4

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "tic_tac_toe.h"
 
 using std::string;
@@ -25,7 +26,7 @@ void tic_tac_toe::start_game(string first_player) {
 }
 
 bool tic_tac_toe::mark_board(int position) {
-    if (board[position - 1] != " " || position < 1 || position > 9)
+    if (board[position - 1] != " " || position < 1 || position > board.size())
         return false;
     board[position - 1] = player;
     set_next_player();
@@ -65,23 +66,24 @@ void tic_tac_toe::set_winner() {
 }
 
 std::ostream &operator<<(std::ostream &out, const tic_tac_toe &game) {
-    out << "\n===\n";
+    string sep((int) sqrt(game.board.size()), '=');
+    out << std::endl << sep << std::endl;
     for (int i = 1; i <= game.board.size(); i++) {
         out << game.board[i - 1];
-        if (i % 3 == 0) {
+        if (i % (int) sqrt(game.board.size()) == 0) {
             out << "\n";
         }
     }
-    out << "===\n";
+    out << sep << std::endl;
     return out;
 }
 
 std::istream &operator>>(std::istream &in, tic_tac_toe &game) {
-    cout << game.get_player() << " it is your turn, type a number 1-9 to place you peg: ";
+    cout << game.get_player() << " it is your turn, type a number 1-" << game.board.size() << " to place you peg: ";
     int position = 0;
     in >> position;
     while (!game.mark_board(position)) {
-        cout << "Invalid input, please type a number 1-9: ";
+        cout << "Invalid input, please type a number 1-" << game.board.size() << ": ";
         in >> position;
     }
     return in;
